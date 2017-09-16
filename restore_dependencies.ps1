ca943074fd20e9fa5cb4d42db42dbf3b2c81fcdc
Vigilans <vigilans@foxmail.com>
2017-09-16 21:51:26 +0800

    remove json.hpp from repo, it will be downloaded by executing restore_dependencies.ps1

--- restore_dependencies.ps1
+++ restore_dependencies.ps1
@@ -7,8 +7,8 @@ Function Download-PanSEU()
     $response = Invoke-RestMethod -Uri $uri -ContentType "application/json" -Method "POST" -Body $body;
     
     $downloadUri = $response.authrequest[1];
-    $webClient = new-object System.Net.WebClient;
-    $webClient.DownloadFile($downloadUri, $Des);
+    Invoke-RestMethod -Uri $downloadUri -Method "GET" -OutFile $Des;
+    Unblock-File $Des;
 }
 
 $src = "https://pan.seu.edu.cn/#/link/58C4D436EFE24B1405861915C48B9A76";
@@ -24,3 +24,15 @@ if (!(Test-Path ./EGE))
     Remove-Item ./EGE.zip
 }
 
+if (!(Test-Path ./nlohmann_json))
+{
+    mkdir nlohmann_json
+}
+
+if (!(Test-Path ./nlohmann_json/json.hpp))
+{
+    $downloadUri = "https://github.com/nlohmann/json/releases/download/v2.1.1/json.hpp"
+    $des = "./nlohmann_json/json.hpp"
+    Invoke-RestMethod -Uri $downloadUri -Method "GET" -OutFile $des;
+    Unblock-File $des;
+}
