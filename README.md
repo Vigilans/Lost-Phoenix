e8cc9cb680243e8408a44fe358160d0ee0580e09
Vigilans <vigilans@foxmail.com>
2018-11-27 18:44:00 +0800

    Update readme.md and push document as readme.pdf

--- README.md
+++ README.md
@@ -7,3 +7,17 @@ https://vigilans-yea.github.io/Lost-Phoenix/
 ### 使用组件
 * [misakamm/xege](https://github.com/misakamm/xege)
 * [nlohmann/json](https://github.com/nlohmann/json)
+
+### 演示截图
+
+![](demo/menu.png)
+
+![](demo/game1.png)
+
+![](demo/game2.png)
+
+![](demo/game3.png)
+
+![](demo/over1.png)
+
+![](demo/over2.png)
--- pkg_install.ps1
+++ pkg_install.ps1
@@ -14,6 +14,8 @@ Function Download-PanSEU()
 $src = "https://pan.seu.edu.cn/#/link/58C4D436EFE24B1405861915C48B9A76";
 $des = "./EGE.zip";
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 if (!(Test-Path ./EGE)) 
 {
     if (!(Test-Path ./EGE.zip))
@@ -31,8 +33,10 @@ if (!(Test-Path ./nlohmann_json))
 
 if (!(Test-Path ./nlohmann_json/json.hpp))
 {
-    $downloadUri = "https://github.com/nlohmann/json/releases/download/v2.1.1/json.hpp"
+    $downloadUri = "https://raw.githubusercontent.com/nlohmann/json/7b2f8cce0322bea2d8112635e15c63742ed92e2f/include/nlohmann/json.hpp"
     $des = "./nlohmann_json/json.hpp"
     Invoke-RestMethod -Uri $downloadUri -Method "GET" -OutFile $des;
     Unblock-File $des;
 }
+
+pause
